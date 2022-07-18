4121b78b60ce47a7dca95d1192e1e7cb401ec185
lukemartinlogan <lukemartinlogan@gmail.com>
2022-07-18 10:50:42 -0500

    Add most of labios dependencies.

---
+++ packages/nats-c/package.py
@@ -0,0 +1,47 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install nats-c
+#
+# You can edit this file again by typing:
+#
+#     spack edit nats-c
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class NatsC(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url      = "https://github.com/nats-io/nats.c/archive/refs/tags/v3.3.0.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('3.3.0', sha256='16e700d912034faefb235a955bd920cfe4d449a260d0371b9694d722eb617ae1')
+
+    # FIXME: Add dependencies if required.
+    depends_on('protobuf-c')
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
