cddf7c078d4e57fb7eddef7cfc31455b541d02b7
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-09 03:01:12 -0500

    v2.0.3 daos

--- packages/daos/package.py
+++ packages/daos/package.py
@@ -38,7 +38,7 @@ class Daos(Package):
     maintainers = ['lukemartinlogan']
 
     # FIXME: Add proper versions and checksums here.
-    version('2.0.3', git="https://github.com/daos-stack/daos.git", branch='release/2.0.3', submodules=True)
+    version('2.0.3', git="https://github.com/daos-stack/daos.git", branch='v2.0.3', submodules=True)
     version('2.0', git="https://github.com/daos-stack/daos.git", branch='release/2.0', submodules=True)
     phases = ["build", "install"]
 
