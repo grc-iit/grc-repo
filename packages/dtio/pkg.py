03570f656a7e5c4d17b9f8fad0e4d6066d4a6c31
lukemartinlogan <lukemartinlogan@gmail.com>
2025-07-23 21:12:00 -0500

    Add dtio

---
+++ packages/dtio/pkg.py
@@ -0,0 +1,32 @@
+from spack.package import *
+
+class Dtio(CMakePackage):
+    homepage = "https://github.com/grc-iit/DTIO"
+    git = "git@github.com:grc-iit/DTIO.git"
+
+    version('main', branch='main', submodules=True, preferred=True)
+
+    variant('mpi', default=True, description='Enable MPI support')
+    variant('liburing', default=False, description='Enable liburing support')
+    variant('nocompile', default=False, description='Do not compile the library (used for dev purposes)')
+
+    depends_on('iowarp-runtime')
+    depends_on('hdf5')
+    depends_on('liburing', when='+liburing')
+    depends_on('mpi', when='+mpi')
+
+    def cmake_args(self):
+        args = []
+        if '+mpi' in self.spec:
+            args.append(self.define('DTIO_ENABLE_MPI', 'ON'))
+        else:
+            args.append(self.define('DTIO_ENABLE_MPI', 'OFF'))
+        if '+liburing' in self.spec:
+            args.append(self.define('DTIO_ENABLE_LIBURING', 'ON'))
+        else:
+            args.append(self.define('DTIO_ENABLE_LIBURING', 'OFF'))
+        if '+nocompile' in self.spec:
+            args.append(self.define('DTIO_NO_COMPILE', 'ON'))
+        else:
+            args.append(self.define('DTIO_NO_COMPILE', 'OFF'))
+        return args 
